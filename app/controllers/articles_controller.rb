765045d0c7bc281b4a007f0e98e2fe4b9de8f121
Miłosz Sędziak <m.sedziak01@gmail.com>
2016-11-09 13:27:29 +0100

    Added update (edit articles) ability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,18 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
-  end
-
-  def show
-    @article = Article.find(params[:id])
+    @articles = Article.all.reverse
   end
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
